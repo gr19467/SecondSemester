716059c522dce155689f3e2404cb10fcae369759
Gaby <gr19467@ms.wasatch.edu>
2023-01-24 09:11:18 -0700

    End of class 1/24/23
    -done

--- src/IterativeSorting.java
+++ src/IterativeSorting.java
@@ -50,10 +50,13 @@ public class IterativeSorting {
         for (int i = 1; i < arr.length-1; i++) {
 
             key = arr[i];
+            int j = i-1;
 
-            for (int j = arr.length-1; j >= 0 && arr[j] > key; j--) {
-
+            while(j >=0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
             }
+            arr[j+1] = key;
         }
     }
 }
